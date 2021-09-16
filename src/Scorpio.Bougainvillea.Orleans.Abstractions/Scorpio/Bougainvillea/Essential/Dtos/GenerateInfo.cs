3bee9ac00d595066889e9cbb1909fb7b94732bf3
王子丹 <zidan.wang@live.com>
2021-09-16 16:26:25 +0800

    临时提交。

---
+++ src/Scorpio.Bougainvillea.Orleans.Abstractions/Scorpio/Bougainvillea/Essential/Dtos/GenerateInfo.cs
@@ -0,0 +1,48 @@
+﻿namespace Scorpio.Bougainvillea.Essential.Dtos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GenerateInfo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public long AvatarId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ServerId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Image { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Sex { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int HeadId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int SkinId { get; set; }
+    }
+}
--- src/Scorpio.Bougainvillea.Orleans.Abstractions/Scorpio/Bougainvillea/Essential/IAvatar.cs
+++ src/Scorpio.Bougainvillea.Orleans.Abstractions/Scorpio/Bougainvillea/Essential/IAvatar.cs
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 using Orleans;
 
+using Scorpio.Bougainvillea.Essential.Dtos;
+
 namespace Scorpio.Bougainvillea.Essential
 {
     /// <summary>
-    /// 
+    /// 角色
     /// </summary>
     public interface IAvatar: IGrainWithIntegerKey
     {
+        /// <summary>
+        /// 角色是否已创建
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> IsGenerated();
 
+        /// <summary>
+        /// 创建角色
+        /// </summary>
+        /// <param name="generateInfo"></param>
+        /// <returns></returns>
+        Task Generate(GenerateInfo generateInfo);
+
+        /// <summary>
+        /// 创建角色后操作
+        /// </summary>
+        /// <returns></returns>
+        Task PostGenerate();
     }
 }
