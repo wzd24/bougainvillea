f2e363599e4798ea3987c1c48f52f0b5d8590650
Zidan Wang <zidan.wang@live.com>
2021-09-17 17:58:19 +0800

    临时提交

--- src/Scorpio.Bougainvillea.Entities/Scorpio/Bougainvillea/Entities/Avatar/AvatarBase.cs
+++ src/Scorpio.Bougainvillea.Entities/Scorpio/Bougainvillea/Entities/Avatar/AvatarBase.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-namespace Scorpio.Bougainvillea.Essential
+namespace Scorpio.Bougainvillea.Entities.Avatar
 {
 
     /// <summary>
